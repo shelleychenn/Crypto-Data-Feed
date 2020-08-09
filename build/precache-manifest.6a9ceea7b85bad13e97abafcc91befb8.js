449fc90859f13f82e2fb1c8fa39ba0f300eeab14
shelleychenn <shelleychen322@gmail.com>
2020-08-09 12:54:35 -0400

    Initialize file structure

---
+++ build/precache-manifest.6a9ceea7b85bad13e97abafcc91befb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfd74b25749deb0cb993876aa52641b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ecf3f6c29028b27d317",
+    "url": "/static/css/main.ffeecca2.chunk.css"
+  },
+  {
+    "revision": "7105382f0cf698d9fb74",
+    "url": "/static/js/2.3cb25bfb.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.3cb25bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ecf3f6c29028b27d317",
+    "url": "/static/js/main.5f8892ac.chunk.js"
+  },
+  {
+    "revision": "b6376a6473f3d7cb7544",
+    "url": "/static/js/runtime-main.f2368b8b.js"
+  }
+]);
